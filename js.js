5df05266c18fe6dbc72288ee25f4bd6103d21d7e
Maya Ismaiel <maya.ismaiel@gmail.com>
2016-08-09 15:26:04 -0700

    Update #3

--- js.js
+++ js.js
@@ -2,22 +2,20 @@ console.log('hello world');
 
 function setUp(){
 
-console.log('We are setting up the DOM');
-document.getElementById('add').onclick = handleAddButton;
-document.getElementById('todos').onclick = handleNewTask;
+	console.log('We are setting up the DOM');
+	document.getElementById('add').onclick = handleAddButton;
+	document.getElementById('todos').onclick = handleNewTask;
 
 
+	$('body').click(function(){
+		console.log('body clicked');
+	});
 
-$('body').click(function(){
-	console.log('body clicked');
-});
-
-$('#myForm').click(function(){
-	console.log('form clicked');
-});
+	$('#myForm').click(function(){
+		console.log('form clicked');
+	});
 
 }
-
 
 function handleAddButton(){
 	console.log('button clicked');
@@ -26,17 +24,14 @@ function handleAddButton(){
 	var newTaskVal = newTask.val();
 	var arrayList = [];
 
-	// list.append("<div>"+newTaskVal+"</div>");
+	//list.append("<div>"+newTaskVal+"</div>");
 
 	list.append('<input type="checkbox" name="todoItem"/>' + newTaskVal +'<br>');
-
-
 
 	//creating an array out of the appended list
 	arrayList.push("<div>"+newTaskVal+"</div>");
 	console.log(arrayList);
 }
-
 
 	//I'm trying to remove a clicked on div item
 
@@ -46,16 +41,12 @@ function handleNewTask(){
 	var newTask = $('#todoItem');
 	var newTaskVal = newTask.val();
 
-	list.remove();
-
-	
+	list.remove();	
 }
 
 function newPage(){
 	window.open('form.php');
 }
-
-
 
 $(document).ready(setUp);
 
