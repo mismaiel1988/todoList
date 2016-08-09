1b547575d606e1f6211092c159b49c6cabad5a98
Maya Ismaiel <maya.ismaiel@gmail.com>
2016-08-09 14:43:56 -0700

    new update

--- js.js
+++ js.js
@@ -4,7 +4,9 @@ function setUp(){
 
 console.log('We are setting up the DOM');
 document.getElementById('add').onclick = handleAddButton;
-document.getElementById('task').onclick = handleNewTask;
+document.getElementById('todos').onclick = handleNewTask;
+
+
 
 $('body').click(function(){
 	console.log('body clicked');
@@ -24,7 +26,11 @@ function handleAddButton(){
 	var newTaskVal = newTask.val();
 	var arrayList = [];
 
-	list.append("<div>"+newTaskVal+"</div>");
+	// list.append("<div>"+newTaskVal+"</div>");
+
+	list.append('<input type="checkbox" name="todoItem"/>' + newTaskVal +'<br>');
+
+
 
 	//creating an array out of the appended list
 	arrayList.push("<div>"+newTaskVal+"</div>");
@@ -37,15 +43,17 @@ function handleAddButton(){
 function handleNewTask(){
 	console.log('handleNewTask');
 	var list = $('#todos');
-	var newTask = $('#task');
+	var newTask = $('#todoItem');
 	var newTaskVal = newTask.val();
 
-	list.remove(newTaskVal);
+	//list.remove();
 
 	
 }
 
-
+function newPage(){
+	window.open('form.php');
+}
 
 
 
