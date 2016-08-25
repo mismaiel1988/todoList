130592d4cde497e1283aad030cf067dfd29b6c04
Maya Ismaiel <maya.ismaiel@gmail.com>
2016-08-24 19:05:27 -0700

    Reindented Code

--- js.js
+++ js.js
@@ -1,11 +1,8 @@
 console.log('hello world');
 
 function setUp(){
-
 	$("#add").click(handleAddButton);
-
 	$("ul").on("click",'input', handleCheckBoxes);
-
 }
 
 function handleAddButton(){
@@ -18,7 +15,7 @@ function handleAddButton(){
 
   var newElement = $('<li class="newItem"><input type="checkbox" name="todoItem" value= '+ newTaskVal +' ><label>' + newTaskVal +'</label></li>');
 
-  if (newTaskVal.length > 0){
+  if(newTaskVal.length > 0){
     // We are hiding first because the newly added element has
     // to be appended in the invisible state for the fade in effect to work.
     newElement.hide();
@@ -34,40 +31,29 @@ function handleAddButton(){
   newTask.val('');
 }
 
-	//I'm trying to remove a clicked on div item
-
-function afterFadeOut(){
-
-}
-
 function handleCheckBoxes(event){
-	console.log('handleNewTask');
-	var list = $('#todos');
-	var newTask = $('#todoItem');
-	var newTaskVal = newTask.val();
+ console.log('handleNewTask');
+ var list = $('#todos');
+ var newTask = $('#todoItem');
+ var newTaskVal = newTask.val();
 
-	var cList =  $('.completed');
+ var cList =  $('.completed');
 
-    //$(this).children('input').prop('checked',true);
+  //$(this).children('input').prop('checked',true);
 
-  if($(this).prop('checked'))
-  {
-  	// $(this).parent().hide();
+  if($(this).prop('checked')){
 
     var liElt = $(this).parent();
-  	liElt.fadeOut('slow', function(){
-    	cList.append(liElt);
-  	    liElt.fadeIn('slow');
-  	});
-
+    liElt.fadeOut('slow', function(){
+  	  cList.append(liElt);
+      liElt.fadeIn('slow');
+    });
+    }
+    else{
+     $(this).parent().hide();
+     list.append($(this).parent());
+     $(this).parent().fadeIn('slow');
   }
-   else{
-   $(this).parent().hide();
-   	list.append($(this).parent());
-   	$(this).parent().fadeIn('slow');
-   }
-	//$(this).remove();
-
 }
 
 $(document).ready(setUp);
